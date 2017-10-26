80be6e47afa0e752d34100f7f61b391d2597807e
Kasper Hauschidt <kaspervhauschildt@gmail.com>
2017-10-26 13:10:58 +0200

    added project types

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025113941) do
+ActiveRecord::Schema.define(version: 20171026091928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "educations", force: :cascade do |t|
     t.integer "year"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "project_types", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

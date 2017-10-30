27bc131c14929080ddd6dfa6357dc0001ae14458
Kasper Hauschidt <kaspervhauschildt@gmail.com>
2017-10-30 12:58:25 +0100

    trying to fix s3 issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.paperclip_defaults = {
-    storage: :s3,
-    s3_credentials: {
-    bucket: ENV.fetch('kaspervh'),
-    access_key_id: ENV.fetch('AKIAIPW36MGQKAG3W5DQ'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('eu-west-2'),
+   config.paperclip_defaults = {
+   storage: :s3,
+   s3_credentials: {
+   bucket: ENV.fetch('S3_BUCKET_NAME'),
+   access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+   secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+   s3_region: ENV.fetch('AWS_REGION'),
+   }
   }
-}
 end

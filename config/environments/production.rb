c12469b8b63c2a125af7bc9c30bda2be9ff6d55a
kaspervh <kaspervhauschildt@gmail.com>
2018-06-29 09:37:27 +0200

    did stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
+
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = ":class/:attachment/:id_partition/:style/:filename"
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-east-2.amazonaws.com'
 end
